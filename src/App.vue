f67418c5f12098ca6e0d365a266c2fd6a61ce485
Shameel Fazul <shameel.fazul@mail.com>
2021-02-17 17:08:56 +0530

    Modal Project v1
    - Emitting Custom Events
    - Event Modifiers
    - Slots - Default & Named Slots
    - Teleport Elements

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,32 @@
 <template>
   <h1>{{ title }}</h1>
+  <br>
   <input type="text" ref="name">
   <button @click="handleClick">click me</button>
-  <Modal :header="header" text="Grab your ninja swag for half price" theme="sale" /> <!-- Sending props to other components - single source of truth (SSOT) --> <!-- Use data binding to add dynamic values or arrays; ex: header -->
+  <teleport to=".modals" v-if="showModal"> <!-- Teleports this to anywhere in the DOM; ex: to the div with the .modals class in index -->
+    <Modal :header="header" text="Grab your ninja swag for half price" theme="sale" @close="toggleModal"> <!-- Sending props to other components - single source of truth (SSOT) --> <!-- Use data binding to add dynamic values or arrays; ex: header -->
+     <template v-slot:links> <!-- Slot directive to assign a custom slot name -->
+       <a href="#">Sign up now</a> <!-- Named slots to organize the template in the child component -->
+       <a href="#">more info</a>
+     </template>
+     <h1>Slot Example</h1>  <!-- default slot -->
+     <p>Slots are similar to props, but it's used to send templates to child components</p>
+    </Modal>
+  </teleport>
+  <br>
+  <button @click.alt="toggleModal">toggle modal (event modifier [alt])</button>
+  <br>
+  <p>Cookie Modal</p>
+  <div v-if="showCookieModal">
+    <Modal :header="cookieTitle" :text="CookieContent" @close="toggleCookieModal">
+      <template v-slot:links>
+        <a href="#" @click="toggleCookieModal">Agree</a>
+         <a href="#" @click="toggleCookieModal">Disagree</a>
+      </template>
+      <p>According to internet privacy laws, we need your permission before storing data for better user experience</p>
+    </Modal>
+  </div>
+  <button @click="toggleCookieModal">toggle cookie modal</button>
 </template>
 
 <script>
@@ -14,7 +38,11 @@ export default {
   data() {  // Component Object
     return {
       title: 'Shameel Fazul',
-      header: "Sign up for the giveaway"
+      header: "Sign up for the giveaway",
+      showModal: false,
+      cookieTitle: 'Privacy',
+      cookieContent: 'This site stores data in your device',
+      showCookieModal: false
     }
   },
   methods: {
@@ -22,13 +50,19 @@ export default {
       console.log(this.$refs.name) // referencing DOM elements. Template Refs; like queryselector
       this.$refs.name.classList.add('active') //adding the class 'active' to the input element
        this.$refs.name.focus()
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+     toggleCookieModal() {
+      this.showCookieModal = !this.showCookieModal
     }
   }
 }
 </script>
 
 <style> /** <style scoped> to only apply styles to this component */
-#app {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,15 +1,24 @@
 <template>
-    <div class="backdrop">
+    <div class="backdrop" @click.self="closeModal">
         <div class="modal" :class="{ sale: theme === 'sale' }">
           <h1>{{ header }}</h1>
             <p>{{ text }}</p>
+            <slot></slot> <!-- The slot (template) sent from the parent component will be registered here -->
+            <div class="actions">
+              <slot name="links"></slot> <!-- Custom named slots to organize it well in the child component -->
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-  props: ['header', 'text', 'theme'] // Obtaining the props sent to this component
+  props: ['header', 'text', 'theme'], // Obtaining the props sent to this component
+  methods: {
+    closeModal() {
+      this.$emit('close') // Emitting Custom Event, which can be listened from the parent component
+    }
+  }
  }
 </script>
 
@@ -33,6 +42,24 @@ export default {
     color: white;
   }
   .modal.sale h1 {
+    color: white;
+  }
+  .modal .actions {
+    text-align: center;
+    margin: 30px 0 10px 0;
+  }
+  .modal .actions a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+  }
+  .modal .sale .actions {
+    color: white;
+  }
+  .modal .sale .actions a {
     color: white;
   }
 </style>
